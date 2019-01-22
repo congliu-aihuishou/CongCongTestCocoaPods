1d003462e9a0b0c4515cce1ee00c0b88f8bf5747
cong.liu <cong.liu@aihuishou.com>
2019-01-22 10:24:29 +0800

    add files

---
+++ CCCreatePodsLib/Classes/UIView+Size.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Size.swift
+//  TestMyCocoapods
+//
+//  Created by CongCong on 2019/1/22.
+//  Copyright © 2019 CongCong. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIView {
+    
+    var width: CGFloat {
+        return self.frame.size.width
+    }
+
+    var height: CGFloat {
+        return self.frame.size.height
+    }
+}
---
+++ Example/Pods/Target Support Files/CCCreatePodsLib/CCCreatePodsLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CCCreatePodsLib : NSObject
+@end
+@implementation PodsDummy_CCCreatePodsLib
+@end
